85eef72a8de8d1f3f3dae4c50b889a34bc3323e7
magdyMohammed20 <magdymohammed37@yahoo.com>
2024-10-06 14:48:15 +0300

    Create What Do You Get

--- ar.js
+++ ar.js
@@ -2,5 +2,14 @@ let ar = {
     main: "الرئيسية",
     services: "الخدمات",
     what_we_do: "ماذا نفعل",
-    contact_us: "تواصل معنا"
+    contact_us: "تواصل معنا",
+    what_do_you_get: "ماذا ستحصل عليه عند استخدام الخزامي؟",
+    complete_security_in_transactions: "أمان تام في التعاملات",
+    security_card_p: "نقدم منصات آمنة لضمان حماية معلوماتك الشخصية.",
+    connect_with_organizations: "تواصل مع المنظمات",
+    connect_with_organizations_p: "تسهيل الاتصال بينك وبين الجهات المستفيدة لمتابعة احتياجاتهم.",
+    tangible_impact_of_projects: "تأثير ملموس للمشاريع ",
+    tangible_impact_of_projects_p: "نضمن لك تقارير تفصيلية حول تأثير تبرعاتك على المشاريع.",
+    facilitating_donation_processes: "تيسير عمليات التبرع",
+    facilitating_donation_processes_p: "نقدم لك نظامًا سهل الاستخدام لجمع التبرعات بسرعة وفعالية."
 };
--- en.js
+++ en.js
@@ -2,5 +2,14 @@ let en = {
     main: "Main",
     services: "Services",
     what_we_do: "What we do",
-    contact_us: "Contact us"
+    contact_us: "Contact us",
+    what_do_you_get: "What do you get when you use lavender?",
+    complete_security_in_transactions: "Complete security in transactions",
+    security_card_p: "We provide secure platforms to ensure the protection of your personal information.",
+    connect_with_organizations: "Connect with organizations",
+    connect_with_organizations_p: "Facilitating communication between you and the beneficiaries to follow up on their needs.",
+    tangible_impact_of_projects: "Tangible impact of projects",
+    tangible_impact_of_projects_p: "We guarantee you detailed reports on the impact of your donations on projects.",
+    facilitating_donation_processes: "Facilitating donation processes",
+    facilitating_donation_processes_p: "We provide you with an easy-to-use system for collecting donations quickly and effectively."
 }
