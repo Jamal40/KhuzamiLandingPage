06e32c8859814aabbc2242da3ec2cd5470741c07
magdyMohammed20 <magdymohammed37@yahoo.com>
2024-10-06 16:48:32 +0300

    Create How We Work Section

--- en.js
+++ en.js
@@ -11,5 +11,7 @@ let en = {
     tangible_impact_of_projects: "Tangible impact of projects",
     tangible_impact_of_projects_p: "We guarantee you detailed reports on the impact of your donations on projects.",
     facilitating_donation_processes: "Facilitating donation processes",
-    facilitating_donation_processes_p: "We provide you with an easy-to-use system for collecting donations quickly and effectively."
+    facilitating_donation_processes_p: "We provide you with an easy-to-use system for collecting donations quickly and effectively.",
+    how_we_work_title: "How we work and what you get when you use our services?",
+    how_we_work_desc: "At Al-Khuzami, we provide an integrated platform for fundraising and improving charitable work. We facilitate online donation processes, allowing donors to support projects that matter to them with ease. When using our services, you will get accurate reports on the impact of your donations, and ongoing support from our team to ensure a better experience.” Seamless and efficient Join us today and be part of a positive change in society."
 }
