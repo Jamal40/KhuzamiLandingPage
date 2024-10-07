da13e1d8fcaf5bc9594e9776dd3724e908b25c52
Sara <Jamal_Ali@outlook.com>
2024-10-07 14:50:53 +0300

    Add English

--- en.js
+++ en.js
@@ -1,24 +1,51 @@
 let en = {
-    main: "Main",
-    services: "Services",
-    what_we_do: "What we do",
-    contact_us: "Contact us",
-    what_do_you_get: "What do you get when you use lavender?",
-    complete_security_in_transactions: "Complete security in transactions",
-    security_card_p: "We provide secure platforms to ensure the protection of your personal information.",
-    connect_with_organizations: "Connect with organizations",
-    connect_with_organizations_p: "Facilitating communication between you and the beneficiaries to follow up on their needs.",
-    tangible_impact_of_projects: "Tangible impact of projects",
-    tangible_impact_of_projects_p: "We guarantee you detailed reports on the impact of your donations on projects.",
-    facilitating_donation_processes: "Facilitating donation processes",
-    facilitating_donation_processes_p: "We provide you with an easy-to-use system for collecting donations quickly and effectively.",
-    how_we_work_title: "How we work and what you get when you use our services?",
-    how_we_work_desc: "At Al-Khuzami, we provide an integrated platform for fundraising and improving charitable work. We facilitate online donation processes, allowing donors to support projects that matter to them with ease. When using our services, you will get accurate reports on the impact of your donations, and ongoing support from our team to ensure a better experience.” Seamless and efficient Join us today and be part of a positive change in society.",
-    welcome_all_title: "We welcome all your inquiries and are happy to serve you. Contact us for any assistance.",
-    welcome_all_desc: "Have a question? We're here to help! Contact us by phone or email. We'd love to hear from you and will respond as quickly as possible. Feel free to ask any questions or share your thoughts.",
-    send_now: "Send now",
-    name: "name",
-    phone: "phone",
-    subject: "subject",
-    message: "message"
-}
+  main: "Home",
+  services: "Services",
+  what_we_do: "What We Do",
+  contact_us: "Contact Us",
+  branding_headline: `Contribute to <span style="color: var(--primary-color)">good</span>, and be part of the <span style="color: var(--primary-color)">change</span>!`,
+  branding_description: `Join "Al Khuzami" and easily and effectively contribute to charitable work. We connect donors with those in need, working together to make a positive impact on the community. Donate today and leave a clear mark with your contribution!`,
+  join_us: "Join Us",
+  what_do_you_get: "What do you get when using Al Khuzami?",
+  complete_security_in_transactions: "Complete Security in Transactions",
+  security_card_p:
+    "We provide secure platforms to ensure the protection of your personal information.",
+  connect_with_organizations: "Connect with Organizations",
+  connect_with_organizations_p:
+    "Facilitating communication between you and beneficiary organizations to monitor their needs.",
+  tangible_impact_of_projects: "Tangible Impact of Projects",
+  tangible_impact_of_projects_p:
+    "We guarantee detailed reports on how your donations impact projects.",
+  facilitating_donation_processes: "Facilitating Donation Processes",
+  facilitating_donation_processes_p:
+    "We provide you with a user-friendly system to collect donations quickly and efficiently.",
+  vision_one_headline: "How We Help Achieve Your Charitable Goals",
+  vision_one_item_1:
+    "We offer an innovative platform for charitable fundraising.",
+  vision_one_item_2:
+    "We provide technical solutions that facilitate communication between donors and those in need.",
+  vision_one_item_3:
+    "We provide periodic reports showing the impact of your donations on projects.",
+  vision_one_item_4:
+    "We work to improve charitable operations to make them more efficient.",
+  vision_one_item_5:
+    "We aim to build an integrated community that contributes to helping others.",
+  how_we_work_title:
+    "How We Work and What You Will Get When Using Our Services",
+  how_we_work_desc:
+    "At Al Khuzami, we offer a comprehensive platform for fundraising and improving charitable work. We facilitate online donation processes, allowing donors to easily support the projects that matter to them. When you use our services, you will receive accurate reports on the impact of your donations, and ongoing support from our team to ensure a smooth and effective experience. Join us today and be part of a positive change in the community.",
+  summary:
+    "Sadeem Al Khuzami seeks to make charitable work more effective by facilitating donations and enhancing communication between donors and those in need. Join us on our journey to make a positive impact in the community.",
+  find_us: "You can find us on:",
+  our_platforms: "Our Platforms",
+  links: "Links",
+  welcome_all_title:
+    "We welcome all your inquiries and are happy to serve you. Contact us for any assistance.",
+  welcome_all_desc:
+    "Have a question? We're here to help! Contact us via phone or email. We'd love to hear from you and will respond as soon as possible. Don't hesitate to ask any questions or share your ideas.",
+  send_now: "Send Now",
+  name: "Name",
+  phone: "Phone Number",
+  subject: "Subject",
+  message: "Message...",
+};
